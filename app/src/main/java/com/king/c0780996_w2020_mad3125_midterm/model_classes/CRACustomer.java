9276b5bd3b85c3ad8fa0c8a96b8830487b9e8c78
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 07:31:20 -0400

    [Raghav]CRA customer class is now complete

--- app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/model_classes/CRACustomer.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/model_classes/CRACustomer.java
@@ -1,6 +1,5 @@
 package com.king.c0780996_w2020_mad3125_midterm.model_classes;
 
-
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Date;
@@ -29,7 +28,8 @@ public class CRACustomer implements Serializable
         public CRACustomer(long sinNumber, String firstName, String lastName, String fullName,
                            Date birthDate, String gender, int age, LocalDate taxFilingDate, double grossIncome, double federalTax,
                            double provincialTax, double cpp, double ei, double rrspContributed, double carryForwardRRSP,
-                           double totalTaxableIncome, double totalTaxPayed) {
+                           double totalTaxableIncome, double totalTaxPayed)
+        {
             this.sinNumber = sinNumber;
             this.firstName = firstName;
             this.lastName = lastName;
@@ -66,81 +66,98 @@ public class CRACustomer implements Serializable
 
         }
 
-    public long getSinNumber() {
+    public long getSinNumber()
+    {
         return sinNumber;
     }
 
-    public void setSinNumber(long sinNumber) {
+    public void setSinNumber(long sinNumber)
+    {
         this.sinNumber = sinNumber;
     }
 
-    public String getFirstName() {
+    public String getFirstName()
+    {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName)
+    {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
+    public String getLastName()
+    {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName)
+    {
         this.lastName = lastName;
     }
 
-    public String getFullName() {
+    public String getFullName()
+    {
         return fullName;
     }
 
-    public void setFullName(String fullName) {
+    public void setFullName(String fullName)
+    {
         this.fullName = fullName;
     }
 
-    public Date getBirthDate() {
+    public Date getBirthDate()
+    {
         return birthDate;
     }
 
-    public void setBirthDate(Date birthDate) {
+    public void setBirthDate(Date birthDate)
+    {
         this.birthDate = birthDate;
     }
 
-    public int getAge() {
+    public int getAge()
+    {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age)
+    {
         this.age = age;
     }
 
-
-
-    public String getGender() {
+    public String getGender()
+    {
         return gender;
     }
 
-    public void setGender(String gender) {
+    public void setGender(String gender)
+    {
         this.gender = gender;
     }
-    
-    public LocalDate getTaxFilingDate() {
+
+    public LocalDate getTaxFilingDate()
+    {
         return taxFilingDate;
     }
 
-    public void setTaxFilingDate(LocalDate taxFilingDate) {
+    public void setTaxFilingDate(LocalDate taxFilingDate)
+    {
         this.taxFilingDate = taxFilingDate;
     }
 
-    public double getGrossIncome() {
+    public double getGrossIncome()
+    {
         return grossIncome;
     }
 
-    public void setGrossIncome(double grossIncome) {
+    public void setGrossIncome(double grossIncome)
+    {
         this.grossIncome = grossIncome;
     }
 
-    public double getTotalTaxableIncome() {
+    public double getTotalTaxableIncome()
+    {
 
         if (getRrspContributed()>getMaxRRSP()){
             this.totalTaxableIncome = getGrossIncome()-(getCpp()+getEi()+getMaxRRSP());
@@ -151,133 +168,173 @@ public class CRACustomer implements Serializable
         return totalTaxableIncome;
     }
 
-    public void setTotalTaxableIncome(double totalTaxableIncome) {
+    public void setTotalTaxableIncome(double totalTaxableIncome)
+    {
         this.totalTaxableIncome = totalTaxableIncome;
     }
 
-    public double getTotalTaxPayed() {
+    public double getTotalTaxPayed()
+    {
 
         this.totalTaxPayed = getFederalTax() + getProvincialTax();
         return totalTaxPayed;
     }
 
-    public void setTotalTaxPayed(double totalTaxPayed) {
+    public void setTotalTaxPayed(double totalTaxPayed)
+    {
         this.totalTaxPayed = totalTaxPayed;
     }
 
 
-    public double getProvincialTax() {
+    public double getProvincialTax()
+    {
 
-        if (getTotalTaxableIncome()<=10582){
+        if (getTotalTaxableIncome()<=10582)
+        {
             this.provincialTax = 0.0;
-        }else if(getTotalTaxableIncome()>=10582.01 && getTotalTaxableIncome()<=43906) {
+        }
+        else if(getTotalTaxableIncome()>=10582.01 && getTotalTaxableIncome()<=43906)
+        {
             this.provincialTax = 0.0 + (getTotalTaxableIncome() - 10582.01) * 0.0505;
-        }else if(getTotalTaxableIncome()>=43906.01 && getTotalTaxableIncome()<=87813) {
+        }
+        else if(getTotalTaxableIncome()>=43906.01 && getTotalTaxableIncome()<=87813)
+        {
             this.provincialTax = 0.0 + (33323.99 * 0.0505) + ((getTotalTaxableIncome()-43906.01) * 0.0915);
-        }else if(getTotalTaxableIncome()>=87813.01 && getTotalTaxableIncome()<=150000){
+        }
+        else if(getTotalTaxableIncome()>=87813.01 && getTotalTaxableIncome()<=150000)
+        {
             this.provincialTax = 0.0 + (33323.99 * 0.0505) + (43906.99 * 0.0915) + ((getTotalTaxableIncome()-87813.01) * 0.1116 );
-        }else if(getTotalTaxableIncome()>=150000.01 && getTotalTaxableIncome()<=220000){//
+        }
+        else if(getTotalTaxableIncome()>=150000.01 && getTotalTaxableIncome()<=220000)
+        {
             this.provincialTax = 0.0 + + (33323.99 * 0.0505) + (43906.99 * 0.0915) + (62186.99 * 0.1116) + ((getTotalTaxableIncome()-150000.01) * 0.1216);
         }
-        else {
-            this.provincialTax = 0.0 + + (33323.99 * 0.0505) + (43906.99 * 0.0915) + (62186.99 * 0.1116) + (69999.99 * 0.1216) + ((getTotalTaxableIncome()-220000.01) * 0.1316);
-        }
+            else
+                {
+                    this.provincialTax = 0.0 + + (33323.99 * 0.0505) + (43906.99 * 0.0915) + (62186.99 * 0.1116) + (69999.99 * 0.1216) + ((getTotalTaxableIncome()-220000.01) * 0.1316);
+                }
 
         return provincialTax;
     }
 
-    public void setProvincialTax(double provincialTax) {
+    public void setProvincialTax(double provincialTax)
+    {
         this.provincialTax = provincialTax;
     }
 
     public double getFederalTax()
     {
-        if (getTotalTaxableIncome()<=12069){
+        if (getTotalTaxableIncome()<=12069)
+        {
             this.federalTax = 0.0;
-        }else if(getTotalTaxableIncome()>=12069.01 && getTotalTaxableIncome()<=47630) {
+        }else if(getTotalTaxableIncome()>=12069.01 && getTotalTaxableIncome()<=47630)
+        {
             this.federalTax = 0.0 + (getTotalTaxableIncome() - 12069.01) * 0.15;
-        }else if(getTotalTaxableIncome()>=47630.01 && getTotalTaxableIncome()<=95259) {
+        }
+        else if(getTotalTaxableIncome()>=47630.01 && getTotalTaxableIncome()<=95259)
+        {
             this.federalTax = 0.0 + (35560.99 * 0.15) + ((getTotalTaxableIncome()-47630.01) * 0.2050);
-        }else if(getTotalTaxableIncome()>=95259.01 && getTotalTaxableIncome()<=147667){
-            this.federalTax = 0.0 + (35560.99 * 0.15) + (47628.99 * 0.2050) + ((getTotalTaxableIncome()-95259.01) * 0.26 );
-        }else if(getTotalTaxableIncome()>=147667.01 && getTotalTaxableIncome()<=210371){
+        }
+        else if(getTotalTaxableIncome()>=95259.01 && getTotalTaxableIncome()<=147667)
+        {
+            this.federalTax = 0.0 + (35560.99 * 0.15) + (47628.99 * 0.2050) + ((getTotalTaxableIncome()-95259.01) * 0.26);
+        }
+        else if(getTotalTaxableIncome()>=147667.01 && getTotalTaxableIncome()<=210371)
+        {
             this.federalTax = 0.0 + + (35560.99 * 0.15) + (47628.99 * 0.2050) + (52407.99 * 0.26) + ((getTotalTaxableIncome()-147667.01) * 0.29);
         }
-        else {
-            this.federalTax = 0.0 + + (35560.99 * 0.15) + (47628.99 * 0.2050) + (52407.99 * 0.26) + (62703.99 * 0.29) + ((getTotalTaxableIncome()-210371.01) * 0.33);
-        }
+        else
+            {
+              this.federalTax = 0.0 + + (35560.99 * 0.15) + (47628.99 * 0.2050) + (52407.99 * 0.26) + (62703.99 * 0.29) + ((getTotalTaxableIncome()-210371.01) * 0.33);
+            }
 
         return federalTax;
     }
 
-    public void setFederalTax(double federalTax) {
+    public void setFederalTax(double federalTax)
+    {
         this.federalTax = federalTax;
     }
 
-    public double getEi() {
+    public double getEi()
+    {
         double eiGrossModel = this.grossIncome;
-        if (eiGrossModel <53100.0){
+        if (eiGrossModel <53100.0)
+        {
             this.ei = eiGrossModel * 0.0162;
-        }else{
-            this.ei = 53100 * 0.0162;
         }
+        else
+            {
+              this.ei = 53100 * 0.0162;
+            }
         return ei;
     }
 
-    public void setEi(double ei) {
+    public void setEi(double ei)
+    {
         this.ei = ei;
     }
 
-    public double getCpp() {
+    public double getCpp()
+    {
 
         double cppGrossModel = this.grossIncome;
-        if(cppGrossModel < 57400.0){
+        if(cppGrossModel < 57400.0)
+        {
             this.cpp = cppGrossModel * 0.0510;
         }
-        else{
-            this.cpp = 57400 * 0.0510;
-        }
+        else
+            {
+                this.cpp = 57400 * 0.0510;
+            }
         return cpp;
     }
 
-    public void setCpp(double cpp) {
+    public void setCpp(double cpp)
+    {
         this.cpp = cpp;
     }
 
-    public double getRrspContributed() {
+    public double getRrspContributed()
+    {
         return rrspContributed;
     }
 
-    public void setRrspContributed(double rrspContributed) {
+    public void setRrspContributed(double rrspContributed)
+    {
         this.rrspContributed = rrspContributed;
     }
 
-    public double getCarryForwardRRSP() {
-
-
+    public double getCarryForwardRRSP()
+    {
         double rrspContri = this.rrspContributed;
 
         this.carryForwardRRSP = getMaxRRSP() - rrspContri;
 
         return carryForwardRRSP;
     }
-    public double getMaxRRSP() {
+
+    public double getMaxRRSP()
+    {
         this.maxRRSP = this.grossIncome * 0.18;
         return maxRRSP;
     }
 
-    public void setMaxRRSP(double maxRRSP) {
+    public void setMaxRRSP(double maxRRSP)
+    {
         this.maxRRSP = maxRRSP;
     }
 
-    public void setCarryForwardRRSP(double carryForwardRRSP) {
+    public void setCarryForwardRRSP(double carryForwardRRSP)
+    {
         this.carryForwardRRSP = carryForwardRRSP;
     }
 
 
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "CRACustomer{" +
                 "personSINNumber=" + sinNumber +
                 ", firstName='" + firstName + '\'' +
