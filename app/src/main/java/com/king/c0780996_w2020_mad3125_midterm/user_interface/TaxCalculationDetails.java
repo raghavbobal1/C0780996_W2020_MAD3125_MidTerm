c32bd1c77bb0c5a0adb5afa90eecb1f97a2ffd27
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 01:19:29 -0400

    [Raghav] added tax calc details class

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/user_interface/TaxCalculationDetails.java
@@ -0,0 +1,7 @@
+package com.king.c0780996_w2020_mad3125_midterm.user_interface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaxCalculationDetails extends AppCompatActivity
+{
+}
