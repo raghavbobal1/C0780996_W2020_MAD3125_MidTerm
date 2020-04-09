a5e9231404bd24d12f80c29c50332c5c05907db9
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 16:51:26 -0400

    [Raghav]Added variables in details entry class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/user_interface/DetailsEntryActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/user_interface/DetailsEntryActivity.java
@@ -2,12 +2,46 @@ package com.king.c0780996_w2020_mad3125_midterm.user_interface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.king.c0780996_w2020_mad3125_midterm.R;
 
 public class DetailsEntryActivity extends AppCompatActivity
 {
+    private Button btn_submit;
+    private Button btn_clear;
+    private Button btn_ok;
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    private TextInputLayout ed_sin;
+    private TextInputEditText ed_sintext;
+
+    private TextInputLayout ed_fname;
+    private TextInputEditText ed_fnametext;
+
+    private TextInputLayout ed_lname;
+    private TextInputEditText ed_lnametext;
+
+    private TextInputLayout ed_dob;
+    private TextInputEditText ed_dobtext;
+
+    private TextInputLayout ed_dof;
+    private TextInputEditText ed_doftext;
+
+    private TextInputLayout ed_grossincome;
+    private TextInputEditText ed_grossincometext;
+
+    private TextInputLayout ed_rrsp;
+    private TextInputEditText ed_rrsptext;
+
+    private TextView textWarning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
