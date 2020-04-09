5e827bdb40734130d29e2926a5d2ff2a276e93c5
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 07:01:26 -0400

    [Raghav] Initialized all the variables

--- app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/model_classes/CRACustomer.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/model_classes/CRACustomer.java
@@ -24,6 +24,28 @@ public class CRACustomer implements Serializable {
     public float totalTaxableIncome;
     public float totalTaxPayed;
     public float maxRRSP;
-
+    
+    public CRACustomer(long personSINNumber, String firstName, String lastName, String fullName,
+                       Date birthDate, String gender, int age, LocalDate taxFilingDate, float grossIncome, float federalTax,
+                       float provincialTax, float cpp, float ei, float rrspContributed, float carryForwardRRSP,
+                       float totalTaxableIncome, float totalTaxPayed) {
+        this.sINNumber = personSINNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.age = age;
+        this.taxFilingDate = taxFilingDate;
+        this.grossIncome = grossIncome;
+        this.federalTax = federalTax;
+        this.provincialTax = provincialTax;
+        this.cpp = cpp;
+        this.ei = ei;
+        this.rrspContributed = rrspContributed;
+        this.carryForwardRRSP = carryForwardRRSP;
+        this.totalTaxableIncome = totalTaxableIncome;
+        this.totalTaxPayed = totalTaxPayed;
+    }
 
 }
