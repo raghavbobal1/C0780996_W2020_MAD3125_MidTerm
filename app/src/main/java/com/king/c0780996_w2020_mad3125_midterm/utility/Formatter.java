07938a70e8ebce9a5db3bd9572fc067a468a0174
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 18:18:18 -0400

    [Raghav]Added a formatter class for date and currency formatting

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/utility/Formatter.java
@@ -0,0 +1,27 @@
+package com.king.c0780996_w2020_mad3125_midterm.utility;
+
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class Formatter
+{
+    private static Formatter repoObj = new Formatter();
+    public static Formatter getInstance() {
+        return repoObj;
+    }
+    private Formatter()
+    { }
+
+    public LocalDate stringToDate(String aDate)
+    {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd-MMM-yyyy");
+        return formatter.parseLocalDate(aDate);
+    }
+
+    public String doubleFormatter(Double d)
+    {
+        return String.format("%.2f", d);
+    }
+}
+
