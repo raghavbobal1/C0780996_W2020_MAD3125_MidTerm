935a7158ab4d97dd901337b74ac6b7cd576105d3
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-08 22:33:12 -0400

    [Raghav] added a splashscreen class

---
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/user_interface/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.king.c0780996_w2020_mad3125_midterm.user_interface;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.king.c0780996_w2020_mad3125_midterm.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this, PersonInformationEntryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },0000);  //Change after testing
+
+    }
+
+}
