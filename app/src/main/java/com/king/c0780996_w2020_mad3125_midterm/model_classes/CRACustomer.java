e5f19bb8f7e74a2d3f0e1ed85705176bb38a4c8d
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 06:58:18 -0400

    [Raghav] Added variables in cra customer class

--- app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/model_classes/CRACustomer.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/model_classes/CRACustomer.java
@@ -1,7 +1,29 @@
 package com.king.c0780996_w2020_mad3125_midterm.model_classes;
 
-public class CRACustomer
-{
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class CRACustomer implements Serializable {
+    public long sINNumber;
+    public String firstName;
+    public String lastName;
+    public String fullName;
+    public Date birthDate;
+    public String gender;
+    public int age;
+    public LocalDate taxFilingDate;
+    public float grossIncome;
+    public float federalTax;
+    public float provincialTax;
+    public float cpp;
+    public float ei;
+    public float rrspContributed;
+    public float carryForwardRRSP;
+    public float totalTaxableIncome;
+    public float totalTaxPayed;
+    public float maxRRSP;
 
 
 }
