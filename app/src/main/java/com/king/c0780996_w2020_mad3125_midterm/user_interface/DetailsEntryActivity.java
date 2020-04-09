d5e1ff9b8632aa1b831153b2f11c81ed9ff5f7eb
raghavbobal1 <raghavbobal001@gmail.com>
2020-04-09 06:49:01 -0400

    [Raghav] Added id's for every textview

--- app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/user_interface/DetailsEntryActivity.java
+++ app/src/main/java/com/king/c0780996_w2020_mad3125_midterm/user_interface/DetailsEntryActivity.java
@@ -1,4 +1,17 @@
 package com.king.c0780996_w2020_mad3125_midterm.user_interface;
 
-public class DetailsEntryActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.king.c0780996_w2020_mad3125_midterm.R;
+
+public class DetailsEntryActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
